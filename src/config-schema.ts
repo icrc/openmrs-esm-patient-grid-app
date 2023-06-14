0cd24eca0996cbdbb8bc26367e7eb5465f5fb50f
Pedro Sousa <pedro.sousa@merkle.com>
2023-06-14 17:00:39 +0100

    Allow specifying hidden columns by configuration

--- src/config-schema.ts
+++ src/config-schema.ts
@@ -6,8 +6,27 @@ export const configSchema = {
     _description: 'The UUID to be used for age ranges in patient grids.',
     _default: undefined,
   },
+  gridFormConfig: {
+    _type: Type.Array,
+    _description: 'List of forms for this category.',
+    _elements: {
+      formUuid: {
+        _type: Type.UUID,
+        _description: 'UUID of form',
+      },
+      defaultHiddenQuestionIds: {
+        _type: Type.Array,
+      },
+    },
+  },
 };
 
 export type Config = Record<string, unknown> & {
   ageRangeEncounterTypeUuid?: string;
+  gridFormConfig?: Array<GridFormConfig>;
 };
+
+export interface GridFormConfig {
+  formUuid: string;
+  defaultHiddenQuestionIds: string;
+}
