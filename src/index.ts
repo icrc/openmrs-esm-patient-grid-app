78886c75aaa918d854a3239158d81062a80f1e06
Jose Francisco <josecarlos.francisco@emea.merkleinc.com>
2023-07-18 16:41:18 +0100

    migration to core v5

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,17 @@
 import { defineConfigSchema, getAsyncLifecycle, openmrsFetch, registerBreadcrumbs } from '@openmrs/esm-framework';
-import { PatientGridGet } from './api';
 import { configSchema } from './config-schema';
+import { PatientGridGet } from './api';
 
-declare let __VERSION__: string;
-const version = __VERSION__;
-/**
- * This tells the app shell how to obtain translation files: that they
- * are JSON files in the directory `../translations` (which you should
- * see in the directory structure).
- */
-const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
+export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
-const backendDependencies = {
-  fhir2: '^1.2.0',
-  'webservices.rest': '^2.2.0',
+const basePath = `${window.spaBase}/patient-grids`;
+const moduleName = '@icrc/esm-patient-grid-app';
+const options = {
+  featureName: 'patientgrid-app',
+  moduleName,
 };
 
-function setupOpenMRS() {
-  const basePath = `${window.spaBase}/patient-grids`;
-  const moduleName = '@icrc/esm-patient-grid-app';
-  const options = {
-    featureName: 'patientgrid-app',
-    moduleName,
-  };
-
+function startupApp() {
   defineConfigSchema(moduleName, configSchema);
 
   registerBreadcrumbs([
@@ -41,26 +29,7 @@ function setupOpenMRS() {
       parent: basePath,
     },
   ]);
-
-  return {
-    pages: [
-      {
-        load: getAsyncLifecycle(() => import('./Root'), options),
-        route: 'patient-grids',
-        online: true,
-        offline: true,
-      },
-    ],
-    extensions: [
-      {
-        id: 'patient-grids-link',
-        slot: 'app-menu-slot',
-        load: getAsyncLifecycle(() => import('./AppMenuLink'), options),
-        online: true,
-        offline: true,
-      },
-    ],
-  };
 }
 
-export { backendDependencies, importTranslation, setupOpenMRS, version };
+export const patientGridsLink = getAsyncLifecycle(() => import('./AppMenuLink'), options);
+export const root = getAsyncLifecycle(() => import('./Root'), options);
